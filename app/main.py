f76f4a93d566bd84ac04a807bdbdb491cd3f2710
Jakub <jakubmichalski89@fgmail.com>
2025-05-20 14:17:49 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -7,20 +7,32 @@ class Person:
         Person.people[self.name] = self
 
 
-def create_person_list(people: list) -> list:
+def create_person_list(people_data: list) -> list:
+    Person.people = {}
+
     person_list = []
-    for person_dict in people:
+
+    for person_dict in people_data:
         person = Person(person_dict["name"], person_dict["age"])
         person_list.append(person)
 
-    for person_dict in people:
-        person = Person.people[person_dict["name"]]
+    for person_dict in people_data:
+        current_person = Person.people[person_dict["name"]]
+
         if "wife" in person_dict and person_dict["wife"] is not None:
             wife_name = person_dict["wife"]
-            person.wife = Person.people[wife_name]
+            if wife_name in Person.people:
+                current_person.wife = Person.people[wife_name]
+            else:
+                print(
+                    f"Ostrzeżenie: Żona \"{wife_name}\" dla \"{current_person.name}\" nie została znaleziona w danych wejściowych.")
+
         if "husband" in person_dict and person_dict["husband"] is not None:
             husband_name = person_dict["husband"]
-            person.husband = Person.people[husband_name]
+            if husband_name in Person.people:
+                current_person.husband = Person.people[husband_name]
+            else:
+                print(
+                    f"Ostrzeżenie: Mąż \"{husband_name}\" dla \"{current_person.name}\" nie został znaleziony w danych wejściowych.")
 
     return person_list
-
